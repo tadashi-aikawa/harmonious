4e331ac0c6e2b80d50512ad1a16dc9ffc7395707
tadashi-aikawa <syou.maman@gmail.com>
2025-04-21 00:36:44 +0900

    second commit

--- app.vue
+++ app.vue
@@ -1,21 +1,5 @@
-<script setup lang="ts">
-import Button from "~/components/ui/button/Button.vue";
-import { useUsers } from "~/composables/useUsers";
-
-const { users, loading, fetchUsers } = useUsers();
-</script>
-
 <template>
-  <Button variant="destructive" size="lg" class="m-12" @click="fetchUsers">
-    {{ loading ? "Loading..." : "Fetch Users" }}
-  </Button>
-
-  <div class="flex flex-col items-center">
-    <h1 class="text-2xl font-bold">User List</h1>
-    <ul class="list-disc">
-      <li v-for="user in users" :key="user.id">
-        {{ user.first_name }} {{ user.last_name }}
-      </li>
-    </ul>
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
---
+++ layouts/default.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <p class="bg-green-300 p-2 text-lg font-bold">
+      Some default layout content shared across all pages
+    </p>
+    <slot />
+  </div>
+</template>
